157accbb4fae948007d5691e88a8d87ca0423220
eric <eric@genuineburke.com>
2023-03-18 16:59:29 -0500

    Implemented the images screen

---
+++ images_api.js
@@ -0,0 +1,91 @@
+var imageFile = null;
+var imageObject = null;
+
+function drawImages(imagesData) {
+    const canvas = document.getElementById("projectionCanvas");
+    const ctx = canvas.getContext("2d");
+
+    if (!areFilesEqual(imagesData.imageFile, imageFile)) {
+        imageFile = imagesData.imageFile;
+        const reader = new FileReader();
+
+        reader.onload = function (event) {
+            imageObject = new Image();
+            imageObject.src = event.target.result;
+
+            imageObject.onload = function () {
+                drawTiledImage(canvas, ctx, imagesData);
+            };
+        };
+
+        reader.readAsDataURL(imageFile);
+    } else if (imageObject) {
+        drawTiledImage(canvas, ctx, imagesData);
+    }
+}
+
+function areFilesEqual(file1, file2) {
+    if (file1 === null && file2 === null) {
+        return true;
+    }
+
+    if (file1 === null || file2 === null) {
+        return false;
+    }
+
+    return file1.name === file2.name &&
+        file1.size === file2.size &&
+        file1.type === file2.type &&
+        file1.lastModified === file2.lastModified;
+}
+
+
+function drawTiledImage(canvas, ctx, imagesData) {
+    const size = imagesData.size;
+    const angle = imagesData.angle;
+    const mirror = imagesData.mirror;
+    const solo = imagesData.solo;
+
+    const scale = (canvas.width * (size / 100)) / imageObject.width;
+    const maxLen = Math.sqrt(Math.pow(imageObject.width/scale, 2) + Math.pow(imageObject.height/scale, 2));
+
+    const scaledImgWidth = imageObject.width * scale;
+    const scaledImgHeight = imageObject.height * scale;
+
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+
+    ctx.fillStyle = imagesData.backgroundColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.save();
+    ctx.translate(centerX, centerY);
+    ctx.rotate(angle * Math.PI / 180);
+    ctx.scale(scale, scale);
+    ctx.translate(-centerX, -centerY);
+
+    if (mirror) {
+        ctx.scale(-1, 1);
+        ctx.translate(-canvas.width, 0);
+    }
+
+    if (solo) {
+        ctx.translate(-imageObject.width / 2, -imageObject.height / 2);
+        ctx.drawImage(imageObject, centerX, centerY);
+    } else {
+        const pattern = ctx.createPattern(imageObject, "repeat");
+        ctx.fillStyle = pattern;
+
+        dx = (canvas.width - imageObject.width) / 2;
+        dy = (canvas.height - imageObject.height) / 2;
+
+        // This translation ensures the tiled pattern is always centered in the screen
+        ctx.translate(dx, dy);
+
+        const overdraw = maxLen/2;
+        ctx.fillRect(-overdraw, -overdraw, 
+            canvas.width + (Math.abs(dx) * 2) + (overdraw*2), canvas.height + (Math.abs(dy) * 2) + (overdraw*2));
+
+    }
+    ctx.restore();
+}
