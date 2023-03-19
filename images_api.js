81c5bd6618fcbd2f35e026971783b867a0f059f3
eric <eric@genuineburke.com>
2023-03-19 17:42:37 -0500

    Keep the views in sync as the user navigates

--- images_api.js
+++ images_api.js
@@ -1,26 +1,28 @@
 var imageFile = null;
 var imageObject = null;
 
+// The web page calls this function
 function drawImages(imagesData) {
-    const canvas = document.getElementById("projectionCanvas");
-    const ctx = canvas.getContext("2d");
-
     if (!areFilesEqual(imagesData.imageFile, imageFile)) {
         imageFile = imagesData.imageFile;
-        const reader = new FileReader();
+        if (imageFile) {
+            const reader = new FileReader();
 
-        reader.onload = function (event) {
-            imageObject = new Image();
-            imageObject.src = event.target.result;
+            reader.onload = function (event) {
+                imageObject = new Image();
+                imageObject.src = event.target.result;
 
-            imageObject.onload = function () {
-                drawTiledImage(canvas, ctx, imagesData);
+                imageObject.onload = function () {
+                    drawImagesData(imagesData);
+                };
             };
-        };
 
-        reader.readAsDataURL(imageFile);
-    } else if (imageObject) {
-        drawTiledImage(canvas, ctx, imagesData);
+            reader.readAsDataURL(imageFile);
+        } else {
+            drawImagesData(imagesData);
+        }
+    } else {
+        drawImagesData(imagesData);
     }
 }
 
@@ -39,53 +41,67 @@ function areFilesEqual(file1, file2) {
         file1.lastModified === file2.lastModified;
 }
 
+function drawImagesData(imagesData) {
+    const canvas = document.getElementById("projectionCanvas");
+    const ctx = canvas.getContext("2d");
 
-function drawTiledImage(canvas, ctx, imagesData) {
-    const size = imagesData.size;
-    const angle = imagesData.angle;
-    const mirror = imagesData.mirror;
-    const solo = imagesData.solo;
+    if (imagesData && imagesData.imageFile && imageObject) {
 
-    const scale = (canvas.width * (size / 100)) / imageObject.width;
-    const maxLen = Math.sqrt(Math.pow(imageObject.width/scale, 2) + Math.pow(imageObject.height/scale, 2));
+        const size = imagesData.size;
+        const angle = imagesData.angle;
+        const mirror = imagesData.mirror;
+        const solo = imagesData.solo;
 
-    const scaledImgWidth = imageObject.width * scale;
-    const scaledImgHeight = imageObject.height * scale;
+        const scale = (canvas.width * (size / 100)) / imageObject.width;
+        const maxLen = Math.sqrt(Math.pow(imageObject.width / scale, 2) + Math.pow(imageObject.height / scale, 2));
 
-    const centerX = canvas.width / 2;
-    const centerY = canvas.height / 2;
+        const scaledImgWidth = imageObject.width * scale;
+        const scaledImgHeight = imageObject.height * scale;
 
-    ctx.fillStyle = imagesData.backgroundColor;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+        const centerX = canvas.width / 2;
+        const centerY = canvas.height / 2;
 
-    ctx.save();
-    ctx.translate(centerX, centerY);
-    ctx.rotate(angle * Math.PI / 180);
-    ctx.scale(scale, scale);
-    ctx.translate(-centerX, -centerY);
+        ctx.fillStyle = imagesData.backgroundColor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    if (mirror) {
-        ctx.scale(-1, 1);
-        ctx.translate(-canvas.width, 0);
-    }
+        ctx.save();
+        ctx.translate(centerX, centerY);
+        ctx.rotate(angle * Math.PI / 180);
+        ctx.scale(scale, scale);
+        ctx.translate(-centerX, -centerY);
 
-    if (solo) {
-        ctx.translate(-imageObject.width / 2, -imageObject.height / 2);
-        ctx.drawImage(imageObject, centerX, centerY);
+        if (mirror) {
+            ctx.scale(-1, 1);
+            ctx.translate(-canvas.width, 0);
+        }
+
+        if (solo) {
+            ctx.translate(-imageObject.width / 2, -imageObject.height / 2);
+            ctx.drawImage(imageObject, centerX, centerY);
+        } else {
+            const pattern = ctx.createPattern(imageObject, "repeat");
+            ctx.fillStyle = pattern;
+
+            dx = (canvas.width - imageObject.width) / 2;
+            dy = (canvas.height - imageObject.height) / 2;
+
+            // This translation ensures the tiled pattern is always centered in the screen
+            ctx.translate(dx, dy);
+
+            const overdraw = maxLen / 2;
+            ctx.fillRect(-overdraw, -overdraw,
+                canvas.width + (Math.abs(dx) * 2) + (overdraw * 2), canvas.height + (Math.abs(dy) * 2) + (overdraw * 2));
+
+        }
+        ctx.restore();
     } else {
-        const pattern = ctx.createPattern(imageObject, "repeat");
-        ctx.fillStyle = pattern;
+        // prompt the user to load an image file
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        dx = (canvas.width - imageObject.width) / 2;
-        dy = (canvas.height - imageObject.height) / 2;
-
-        // This translation ensures the tiled pattern is always centered in the screen
-        ctx.translate(dx, dy);
-
-        const overdraw = maxLen/2;
-        ctx.fillRect(-overdraw, -overdraw, 
-            canvas.width + (Math.abs(dx) * 2) + (overdraw*2), canvas.height + (Math.abs(dy) * 2) + (overdraw*2));
-
+        ctx.font = '30px Arial';
+        ctx.textAlign = 'center';
+        ctx.fillStyle = 'white';
+        ctx.fillText("Choose an image file to display.", canvas.width / 2, canvas.height / 2);
     }
-    ctx.restore();
 }
